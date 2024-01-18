b4818ccb73c99d93fc1326c2aac25976b703bc2e
Adam Atanas <adamatanas@gmail.com>
2024-01-18 22:55:35 +0000

    Output training data to h5 file

--- deepreg/model/network.py
+++ deepreg/model/network.py
@@ -3,7 +3,10 @@ from abc import abstractmethod
 from copy import deepcopy
 from typing import Dict, Optional, Tuple
 
+import numpy as np
+import h5py
 import tensorflow as tf
+from tensorflow.python.keras.engine import data_adapter
 
 from deepreg import log
 from deepreg.loss.label import compute_centroid_distance
@@ -364,7 +367,14 @@ class RegistrationModel(tf.keras.Model):
             name=f"metric/{name}_max",
             aggregation="max",
         )
-
+def save_data(moving_image, fixed_image, moving_label, fixed_label, pred_fixed_image, pred_fixed_label, save_idx):
+    with h5py.File(os.path.join("/data1/prj_register/test_save_fit", f'train_{save_idx}.h5'), "w") as f:
+        f["moving_image"]  = moving_image.numpy()
+        f["fixed_image"] = fixed_image.numpy()
+        f["moving_label"] = moving_label.numpy()
+        f["fixed_label"] = fixed_label.numpy()
+        f["pred_fixed_image"] = pred_fixed_image.numpy()
+        f["pred_fixed_label"] = pred_fixed_label.numpy()
 
 @REGISTRY.register_model(name="ddf")
 class DDFModel(RegistrationModel):
@@ -416,6 +426,50 @@ class DDFModel(RegistrationModel):
             config=config,
             name=name,
         )
+
+    
+    def train_step(self, data):
+        """The logic for one training step.
+
+        This method can be overridden to support custom training logic.
+        For concrete examples of how to override this method see
+        [Customizing what happens in fit](
+        https://www.tensorflow.org/guide/keras/customizing_what_happens_in_fit).
+        This method is called by `Model.make_train_function`.
+
+        This method should contain the mathematical logic for one step of
+        training.  This typically includes the forward pass, loss calculation,
+        backpropagation, and metric updates.
+
+        Configuration details for *how* this logic is run (e.g. `tf.function`
+        and `tf.distribute.Strategy` settings), should be left to
+        `Model.make_train_function`, which can also be overridden.
+
+        Args:
+          data: A nested structure of `Tensor`s.
+
+        Returns:
+          A `dict` containing values that will be passed to
+          `tf.keras.callbacks.CallbackList.on_train_batch_end`. Typically, the
+          values of the `Model`'s metrics are returned. Example:
+          `{'loss': 0.2, 'accuracy': 0.7}`.
+        """
+        x, y, sample_weight = data_adapter.unpack_x_y_sample_weight(data)
+        # Run forward pass.
+        with tf.GradientTape() as tape:
+            y_pred = self(x, training=True)
+            loss = self.compute_loss(x, y, y_pred, sample_weight)
+        save_idx = np.random.randint(1000000)
+
+        tf.py_function(save_data, 
+                   [x['moving_image'], x['fixed_image'], x['moving_label'], x['fixed_label'], 
+                    y_pred['pred_fixed_image'], y_pred['pred_fixed_label'], save_idx], 
+                   Tout=[])
+
+        self._validate_target_and_loss(y, loss)
+        # Run backwards pass.
+        self.optimizer.minimize(loss, self.trainable_variables, tape=tape)
+        return self.compute_metrics(x, y, y_pred, sample_weight)
 
     def build_inputs(self) -> Dict[str, tf.keras.layers.Input]:
         """
